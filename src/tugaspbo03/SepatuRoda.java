f8ca10032dea3cc049fd8f6df4f06b0e33f27d74
yafinoor <mnyafi777@gmail.com>
2018-04-10 13:23:42 +0800

    buat Getter and Setter

--- src/tugaspbo03/SepatuRoda.java
+++ src/tugaspbo03/SepatuRoda.java
@@ -10,4 +10,31 @@ public class SepatuRoda {
                 "Merk \t: "+merk+"\n"+
                 "Harga \t:"+harga);
      }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+     
+     
+    
 }
