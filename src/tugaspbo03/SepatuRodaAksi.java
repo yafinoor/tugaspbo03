11cced8cae6b7dce133b864c2f669816106ae565
yafinoor <mnyafi777@gmail.com>
2018-04-10 13:19:48 +0800

    instance, buat atribut dan panggil method

--- src/tugaspbo03/SepatuRodaAksi.java
+++ src/tugaspbo03/SepatuRodaAksi.java
@@ -1,6 +1,11 @@
 package tugaspbo03;
 public class SepatuRodaAksi {
     public static void main(String[] args) {
-        
+         SepatuRoda s = new SepatuRoda();
+         s.warna="Biru Gelap";
+         s.merk="Wimcycle";
+         s.harga=70000;
+         
+         s.cetakinfo();
     }
 }
