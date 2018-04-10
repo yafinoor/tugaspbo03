8b2a2d776fc02dd9fd3e4bb7dac498643770243e
yafinoor <mnyafi777@gmail.com>
2018-04-10 12:59:23 +0800

    Membuat Atribut

--- src/tugaspbo03/SepatuRoda.java
+++ src/tugaspbo03/SepatuRoda.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugaspbo03;
 
-/**
- *
- * @author Muhammad Noor Yafi
- */
 public class SepatuRoda {
-    
+     String warna;
+     String Merk;
+     double harga;
 }
