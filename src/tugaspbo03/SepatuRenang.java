0dc247b114cc8edc707ae22b5dfa139572e04c3e
yafinoor <mnyafi777@gmail.com>
2018-04-10 13:39:40 +0800

    Getter and Setter

--- src/tugaspbo03/SepatuRenang.java
+++ src/tugaspbo03/SepatuRenang.java
@@ -8,6 +8,15 @@ public class SepatuRenang extends SepatuRoda{
    System.out.println("PLEK KEPLEK KEPLEK,SPALSH SPALSH .......");
    
 }
+
+    public String getJenisSepatuRenang() {
+        return jenisSepatuRenang;
+    }
+
+    public void setJenisSepatuRenang(String jenisSepatuRenang) {
+        this.jenisSepatuRenang = jenisSepatuRenang;
+    }
+    
     
     
 }
