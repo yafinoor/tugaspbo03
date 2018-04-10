dff0e70dfdf1911898e82a24c2e757c5826bb468
yafinoor <mnyafi777@gmail.com>
2018-04-10 13:11:47 +0800

    buat method cetakinfo()

--- src/tugaspbo03/SepatuRoda.java
+++ src/tugaspbo03/SepatuRoda.java
@@ -2,6 +2,12 @@ package tugaspbo03;
 
 public class SepatuRoda {
      String warna;
-     String Merk;
+     String merk;
      double harga;
+     
+     void cetakinfo(){
+         System.out.println("Warna \t: "+warna+"\n"+
+                "Merk \t: "+merk+"\n"+
+                "Harga \t:"+harga);
+     }
 }
